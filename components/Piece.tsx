3dcf834ffcdb818a191d61cac884ea2109b82489
Siddhant <siddhantdigraje77@gmail.com>
2025-12-10 23:20:10 +0530

    update components/Piece.tsx

--- components/Piece.tsx
+++ components/Piece.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Piece } from 'chess.js';
 
@@ -7,35 +8,42 @@ interface PieceProps {
   isHidden?: boolean;
   enablePieceRotation?: boolean;
   boardTurn?: 'w' | 'b';
+  isFadingOut?: boolean;
 }
 
 export const pieceToSvg: Record<string, string> = {
-    'wp': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wp.png",
-    'wn': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wn.png",
-    'wb': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wb.png",
-    'wr': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wr.png",
-    'wq': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wq.png",
-    'wk': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wk.png",
-    'bp': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bp.png",
-    'bn': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bn.png",
-    'bb': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bb.png",
-    'br': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/br.png",
-    'bq': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bq.png",
-    'bk': "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bk.png",
+    'wp': "/assets/pieces/default/wp.png",
+    'wn': "/assets/pieces/default/wn.png",
+    'wb': "/assets/pieces/default/wb.png",
+    'wr': "/assets/pieces/default/wr.png",
+    'wq': "/assets/pieces/default/wq.png",
+    'wk': "/assets/pieces/default/wk.png",
+    'bp': "/assets/pieces/default/bp.png",
+    'bn': "/assets/pieces/default/bn.png",
+    'bb': "/assets/pieces/default/bb.png",
+    'br': "/assets/pieces/default/br.png",
+    'bq': "/assets/pieces/default/bq.png",
+    'bk': "/assets/pieces/default/bk.png",
 };
 
 
-export const PieceComponent: React.FC<PieceProps> = ({ piece, onDragStart, isHidden, enablePieceRotation, boardTurn }) => {
+export const PieceComponent: React.FC<PieceProps> = ({ piece, onDragStart, isHidden, enablePieceRotation, boardTurn, isFadingOut }) => {
   const pieceKey = `${piece.color}${piece.type}`;
   const src = pieceToSvg[pieceKey];
 
   const rotationClass = enablePieceRotation && boardTurn === 'b' ? 'rotate-180' : 'rotate-0';
+  
+  // If isHidden is true, opacity-0 applies instantly (unless transition overrides, but we use isHidden for instant hiding)
+  // If isFadingOut is true, we apply opacity-0 with a delay and transition to create the fade effect
+  const fadeClass = isFadingOut 
+    ? 'transition-opacity duration-100 delay-100 opacity-0 ease-out' 
+    : '';
 
   return (
     <div
       draggable="true"
       onDragStart={onDragStart}
-      className={`cursor-grab active:cursor-grabbing w-[85%] h-[85%] z-10 ${rotationClass} ${isHidden ? 'opacity-0' : ''}`}
+      className={`cursor-grab active:cursor-grabbing w-[85%] h-[85%] z-10 ${rotationClass} ${isHidden ? 'opacity-0' : ''} ${fadeClass}`}
       style={{ backgroundImage: `url(${src})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}
       aria-label={`${piece.color === 'w' ? 'White' : 'Black'} ${piece.type}`}
     />
